68933d42f9e96f3022221d2c41db606e2a973cdd
Ana Carolina <125562229+bleer2004@users.noreply.github.com>
2024-10-15 19:18:29 -0300

    Login - design e funcionalidade
    40%

--- src/Login.js
+++ src/Login.js
@@ -1,68 +1,40 @@
-// src/Login.js
-import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
+import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [cpfError, setCpfError] = useState('');
   const navigate = useNavigate();
-  const location = useLocation(); // Importando useLocation
 
-  // Captura o role da URL
-  const params = new URLSearchParams(location.search);
-  const role = params.get('role');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Aqui você pode adicionar a validação do CPF
-    // if (validateCPF(password)) {
-      // Armazena o papel no localStorage
-      localStorage.setItem('role', role);
-      navigate(`/busca?role=${role}`);
-    // } else {
-    //   setCpfError('CPF inválido. Por favor, insira um CPF no formato válido.');
-    // }
+  const handleRoleSelection = (role) => {
+    // Armazenando 
+    localStorage.setItem('role', role);
+    navigate(`/login?role=${role}`);
   };
 
-  // const validateCPF = (cpf) => {
-  //   const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-  //   return cpfRegex.test(cpf);
-  // };
-
   return (
-    <div style={{ maxWidth: '400px', margin: 'auto', padding: '20px', border: '1px solid #ccc' }}>
-      <h2>Preencha seus dados</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Número de registro
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </label>
+    <div className="container">
+      <div className="header">
+        <div className="logo"></div>
+        <div className="title">
+          <h1>Metrô SP - Gestão de equipamentos de combate a incêndio</h1>
         </div>
-        <div>
-          <label>
-            CPF
-            <input
-              type="text"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setCpfError(''); // Limpa o erro quando o usuário começa a digitar
-              }}
-              required
-            />
-          </label>
-          {cpfError && <p style={{ color: 'red' }}>{cpfError}</p>}
+        <div className="company">
+          <h2>CPTM</h2>
+          <p>Identificação</p>
         </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
+      <div className="content">
+        <h3>Escolha seu tipo de usuário</h3>
+        <div className="role-selection">
+          <button onClick={() => handleRoleSelection('admin')}>Administrador</button>
+          <button onClick={() => handleRoleSelection('operador')}>Operador</button>
+          <button onClick={() => handleRoleSelection('leitura')}>Apenas leitura</button>
+        </div>
+        <p className="info-text">
+          Ao escolher um tipo de usuário, você verá apenas seu perfil (privativo). A visualização não
+          terá funcionalidades reais de modificação.
+        </p>
+      </div>
     </div>
   );
 };
