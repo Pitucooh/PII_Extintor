ba538fb0c685bd4b504a9d5b36217fbb4c514a3b
bleer <125562229+bleer2004@users.noreply.github.com>
2024-10-24 23:55:23 -0300

    quase pronto

--- src/Tela_Inicial.js
+++ src/Tela_Inicial.js
@@ -28,23 +28,48 @@ const Tela_Inicial = () => {
       </div>
 
     
-      <div className="role-selection-modal">
-        <div className="role-selection">
-          <button onClick={() => handleRoleSelection('admin')}>Administrador</button>
-          <button onClick={() => handleRoleSelection('operador')}>Operador</button>
-          <button onClick={() => handleRoleSelection('leitura')}>Apenas leitura</button>
+      <div className="light-blue-modal">
+    <p>Este é o modal azul claro, posicionado atrás do modal principal.</p>
+  </div>
 
-          <p className="info-text">
-          Ao escolher um tipo de usuário, você verá apenas seu perfil (privativo). A visualização não
-          terá funcionalidades reais de modificação.
-        </p>
-        </div>
-      </div>
-        <div className="footer">
-          <p>
-            
-          </p>
-        </div>
+  <div className="role-selection-modal">
+    <div className="role-selection">
+      <button onClick={() => handleRoleSelection('admin')}>Administrador</button>
+      <button onClick={() => handleRoleSelection('operador')}>Operador</button>
+      <button onClick={() => handleRoleSelection('leitura')}>Apenas leitura</button>
+
+      <p className="info-text">
+        Ao escolher um tipo de usuário, você verá apenas seu perfil (privativo). A visualização não
+        terá funcionalidades reais de modificação.
+      </p>
+    </div>
+  </div>
+      <footer style={{ 
+              backgroundColor: '#878787', 
+              color: '#fff', 
+              textAlign: 'center', 
+              padding: '10px 20px', 
+              fontSize: '14px',
+              position: 'fixed',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.3)',
+              zIndex: 1000 
+            }}>
+              <p>&copy; 2024 - Todos os direitos reservados.</p>
+              <p>
+                Contact us: <a href="#" style={{ color: '#fff', textDecoration: 'underline' }}>metrocptm.com</a>
+              </p>
+              <div>
+                <a href="https://facebook.com" target="_blank" style={{ color: '#fff', margin: '0 10px' }}>Facebook</a>
+                <a href="https://twitter.com" target="_blank" style={{ color: '#fff', margin: '0 10px' }}>Twitter</a>
+                <a href="https://instagram.com" target="_blank" style={{ color: '#fff', margin: '0 10px' }}>Instagram</a>
+              </div>
+    </footer>
+
+
+
       </div>
   );
 };
