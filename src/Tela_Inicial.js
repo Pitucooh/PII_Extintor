878d01e8c1b6980c317d8f85281a43e924b50353
bleer <125562229+bleer2004@users.noreply.github.com>
2024-10-27 02:12:51 -0300

    TELA DE INICIO PRONTS EEEIIIITAAA O LACRE CIS

--- src/Tela_Inicial.js
+++ src/Tela_Inicial.js
@@ -1,76 +1,97 @@
 // src/Tela_Inicial
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/Tela_Inicial.css';
 
 const Tela_Inicial = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    // Simulate a loading delay of 3 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const handleRoleSelection = (role) => {
-    navigate(`/login?role=${role}`); 
+    navigate(`/login?role=${role}`);
   };
 
   return (
     <div className="container">
-      <div className="header">
-        <div className="logo"></div>
-        <div className="title">
-          <h1>Metrô SP - Gestão de equipamentos </h1>
-          <h1>de combate a incêndio</h1>
+      {isLoading && (
+        <div id="loading-screen">
+          <p>Estamos carregando a tela de login...</p>
+          <div className="balls">
+            <div className="ball"></div>
+            <div className="ball"></div>
+            <div className="ball"></div>
+            
+          </div>
         </div>
-        <div className="company">
-          <h2>CPTM</h2>
-          <p>Identificação</p>
-        </div>
-      </div>
-      <div className="title-modal">
-        <h3>Escolha seu tipo de usuário</h3>
-      </div>
+      )}
 
-    
-      <div className="light-blue-modal">
-    <p>Este é o modal azul claro, posicionado atrás do modal principal.</p>
-  </div>
+      {!isLoading && (
+        <>
+          <div className="header">
+            <div className="logo"></div>
+            <div className="title">
+              <h1>Metrô SP - Gestão de equipamentos</h1>
+              <h1>de combate a incêndio</h1>
+            </div>
+            <div className="company">
+              <img src="./css/logo.JPG" alt="Logo" />
+            </div>
+          </div>
+          <div className="title-modal">
+            <h3>Escolha seu tipo de usuário</h3>
+          </div>
 
-  <div className="role-selection-modal">
-    <div className="role-selection">
-      <button onClick={() => handleRoleSelection('admin')}>Administrador</button>
-      <button onClick={() => handleRoleSelection('operador')}>Operador</button>
-      <button onClick={() => handleRoleSelection('leitura')}>Apenas leitura</button>
+          <div className="light-blue-modal">
+          </div>
 
-      <p className="info-text">
-        Ao escolher um tipo de usuário, você verá apenas seu perfil (privativo). A visualização não
-        terá funcionalidades reais de modificação.
-      </p>
-    </div>
-  </div>
-      <footer style={{ 
-              backgroundColor: '#878787', 
-              color: '#fff', 
-              textAlign: 'center', 
-              padding: '10px 20px', 
-              fontSize: '14px',
-              position: 'fixed',
-              bottom: 0,
-              left: 0,
-              right: 0,
-              boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.3)',
-              zIndex: 1000 
-            }}>
-              <p>&copy; 2024 - Todos os direitos reservados.</p>
-              <p>
-                Contact us: <a href="#" style={{ color: '#fff', textDecoration: 'underline' }}>metrocptm.com</a>
+          <div className="role-selection-modal">
+            <div className="role-selection">
+              <h3>Funções</h3>
+              <button onClick={() => handleRoleSelection('admin')}>Administrador</button>
+              <button onClick={() => handleRoleSelection('operador')}>Operador</button>
+              <h3>Visitante</h3>
+              <button onClick={() => handleRoleSelection('leitura')}>Apenas leitura</button>
+
+              <p className="info-text">
+                Ao escolher APENAS LEITURA, você verá apenas seu perfil (privativo). A visualização não
+                terá funcionalidades reais de modificação.
               </p>
-              <div>
-                <a href="https://facebook.com" target="_blank" style={{ color: '#fff', margin: '0 10px' }}>Facebook</a>
-                <a href="https://twitter.com" target="_blank" style={{ color: '#fff', margin: '0 10px' }}>Twitter</a>
-                <a href="https://instagram.com" target="_blank" style={{ color: '#fff', margin: '0 10px' }}>Instagram</a>
-              </div>
-    </footer>
+            </div>
+          </div>
 
+          <div className="light-blue-modal">
+          </div>
 
-
-      </div>
+          <footer style={{
+            backgroundColor: '#878787',
+            color: '#fff',
+            textAlign: 'center',
+            padding: '10px 20px',
+            fontSize: '14px',
+            position: 'fixed',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.3)',
+            zIndex: 1000
+          }}>
+            <p>&copy; 2024 - Todos os direitos reservados.</p>
+            <p>
+              Contact us: <a href="#" style={{ color: '#fff', textDecoration: 'underline' }}>metrocptm.com</a>
+            </p>
+          </footer>
+        </>
+      )}
+    </div>
   );
 };
 
