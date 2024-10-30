a525bd8f2fe4dd456ab6c92c00beac333921b439
João Cruz <132522413+Pitucooh@users.noreply.github.com>
2024-10-30 15:49:22 -0300

    busca de manutenções

--- src/Busca.js
+++ src/Busca.js
@@ -27,7 +27,11 @@ const QRCodeCanvas = ({ value }) => {
 };
 
 const Busca = () => {
+  const [id, setId] = useState('');
+  const [data, setData] = useState(null);
   const [patrimonio, setPatrimonio] = useState('');
+  const [historico, setHistorico] = useState([]);
+  const [error, setError] = useState('');
   const [resultados, setResultados] = useState([]);
   const [modalData, setModalData] = useState(null);
   const [dadosEditados, setDadosEditados] = useState(null);
@@ -97,6 +101,36 @@ const Busca = () => {
     } catch (error) {
       console.error('Erro:', error);
       setResultadosPredio([{ message: 'Erro ao buscar local' }]);
+    }
+  };
+
+  //--------------------------------------------------BUSCA ID MANUTENÇÃO----------------------------------------------------------------
+  const handle_Manut_Ext = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      const response = await fetch(`http://localhost:3002/manutencao/${id}`);
+      if (!response.ok) throw new Error('Registro não encontrado');
+      const result = await response.json();
+      setData(result);
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    }
+  };
+
+  //--------------------------------------------------BUSCA MANUTENÇÕES POR PATRIMONIO----------------------------------------------------------------
+  const handle_Ext_Manut = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      const response = await fetch(`http://localhost:3002/historico/${patrimonio}`);
+      if (!response.ok) throw new Error('Registro não encontrado');
+      const results = await response.json();
+      setHistorico(results);
+    } catch (err) {
+      setError(err.message);
+      setHistorico([]);
     }
   };
 
@@ -499,10 +533,73 @@ return (
                 ) : null}
               </div>
         </div>
+        <h1>Extintores por Manutenção</h1>
+        <form onSubmit={handle_Manut_Ext}>
+          <label>
+            ID da Manutenção:
+            <input
+              type="text"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Buscar</button>
+        </form>
 
-        {/* Resultados da busca por localizações */}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {data && (
+          <div>
+            <h3>Detalhes da Manutenção de ID {data.id_manutencao}:</h3>
+            <ul>
+              <li>
+                <p>Patrimônio: {data.patrimonio}</p>
+                <p>Data: {data.data_manu}</p>
+                <p>Descrição: {data.desc}</p>
+                <p>Responsável: {data.resp}</p>
+                <p>Observações: {data.observacoes}</p>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <h1>Manutenções por Extintor</h1>
+        <form onSubmit={handle_Ext_Manut}>
+          <label>
+            Patrimônio do Extintor:
+            <input
+              type="text"
+              value={patrimonio}
+              onChange={(e) => setPatrimonio(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Buscar Histórico</button>
+        </form>
+
+        {error && <p>{error}</p>}
+        {historico && historico.manutencoes && historico.manutencoes.length > 0 ? (
+          <div>
+            <h2>Histórico de Manutenções - Patrimônio {historico.patrimonio}</h2>
+            <h3>Total de Manutenções: {historico.manutencoes.length}</h3>  
+            <ul>
+              {historico.manutencoes.map((item) => (
+                <li key={item.manutencaoId}>
+                  
+                  <p><strong>ID:</strong> {item.manutencaoId}</p>
+                  <p><strong>Data:</strong> {item.data}</p>
+                  <p><strong>Responsável:</strong> {item.responsavel}</p>
+                  <p><strong>Descrição:</strong> {item.descricao}</p>
+                  <p><strong>Observações:</strong> {item.observacoes}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          !error && <p>Nenhum dado encontrado</p>
+        )}
+
   
-        <button onClick={handleNavigate}>Relatórios</button> {/* Botão de redirecionamento */}
       </div>
       <Footer></Footer>
     </div>
--- src/Relatorio.js
+++ src/Relatorio.js
@@ -7,40 +7,6 @@ import Navbar from './components/Navbar';
 import './css/Relatorio.css'; // Arquivo CSS atualizado
 
 const Relatorio = () => {
-  const [id, setId] = useState('');
-  const [data, setData] = useState(null);
-  const [patrimonio, setPatrimonio] = useState('');
-  const [historico, setHistorico] = useState([]);
-  const [error, setError] = useState('');
-
-  const handle_Manut_Ext = async (e) => {
-    e.preventDefault();
-    setError('');
-    try {
-      const response = await fetch(`http://localhost:3002/manutencao/${id}`);
-      if (!response.ok) throw new Error('Registro não encontrado');
-      const result = await response.json();
-      setData(result);
-    } catch (err) {
-      setError(err.message);
-      setData(null);
-    }
-  };
-
-  const handle_Ext_Manut = async (e) => {
-    e.preventDefault();
-    setError('');
-    try {
-      const response = await fetch(`http://localhost:3002/historico/${patrimonio}`);
-      if (!response.ok) throw new Error('Registro não encontrado');
-      const results = await response.json();
-      setHistorico(results);
-    } catch (err) {
-      setError(err.message);
-      setHistorico([]);
-    }
-  };
-
   return (
     <div className="layout">
       <Header />
@@ -51,72 +17,6 @@ const Relatorio = () => {
         </div>
       </div>
       <div className="container">
-        <h1>Extintores por Manutenção</h1>
-        <form onSubmit={handle_Manut_Ext}>
-          <label>
-            ID da Manutenção:
-            <input
-              type="text"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-              required
-            />
-          </label>
-          <button type="submit">Buscar</button>
-        </form>
-
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {data && (
-          <div>
-            <h3>Detalhes da Manutenção de ID {data.id_manutencao}:</h3>
-            <ul>
-              <li>
-                <p>Patrimônio: {data.patrimonio}</p>
-                <p>Data: {data.data_manu}</p>
-                <p>Descrição: {data.desc}</p>
-                <p>Responsável: {data.resp}</p>
-                <p>Observações: {data.observacoes}</p>
-              </li>
-            </ul>
-          </div>
-        )}
-
-        <h1>Manutenções por Extintor</h1>
-        <form onSubmit={handle_Ext_Manut}>
-          <label>
-            Patrimônio do Extintor:
-            <input
-              type="text"
-              value={patrimonio}
-              onChange={(e) => setPatrimonio(e.target.value)}
-              required
-            />
-          </label>
-          <button type="submit">Buscar Histórico</button>
-        </form>
-
-        {error && <p>{error}</p>}
-        {historico && historico.manutencoes && historico.manutencoes.length > 0 ? (
-          <div>
-            <h2>Histórico de Manutenções - Patrimônio {historico.patrimonio}</h2>
-            <h3>Total de Manutenções: {historico.manutencoes.length}</h3>  
-            <ul>
-              {historico.manutencoes.map((item) => (
-                <li key={item.manutencaoId}>
-                  
-                  <p><strong>ID:</strong> {item.manutencaoId}</p>
-                  <p><strong>Data:</strong> {item.data}</p>
-                  <p><strong>Responsável:</strong> {item.responsavel}</p>
-                  <p><strong>Descrição:</strong> {item.descricao}</p>
-                  <p><strong>Observações:</strong> {item.observacoes}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          !error && <p>Nenhum dado encontrado</p>
-        )}
-
         <EquipamentosPorRegiao />
         <ValidadeEquipamento />
       </div>
